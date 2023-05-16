7368be5fc45920bbe396d1e54217b377e041a85a
TheDrone7 <h@thedrone7.dev>
2023-05-16 07:15:10 -0400

    Initial commit

---
+++ src/components/drawer.rs
@@ -0,0 +1,25 @@
+use perseus::prelude::*;
+use sycamore::prelude::*;
+
+#[component]
+pub fn Drawer<G: Html>(cx: Scope) -> View<G> {
+    view! { cx,
+      div (class="drawer-side") {
+        label (for="nav-drawer", class="drawer-overlay") {}
+        ul (class="menu p-4 w-80 bg-base-100") {
+          li {
+            a (href="#about") { "About" }
+          }
+          li {
+            a (href="#contact") { "Contact" }
+          }
+          li {
+            a (href="#projects") { "Projects" }
+          }
+          li {
+            a (href="#skills") { "Skills" }
+          }
+        }
+      }
+    }
+}
---
+++ src/template.rs
@@ -0,0 +1,48 @@
+use crate::components::{drawer::Drawer, navbar::Navbar};
+use perseus::prelude::*;
+use serde::{Deserialize, Serialize};
+use sycamore::prelude::*;
+
+#[derive(Serialize, Deserialize, ReactiveState, Clone)]
+#[rx(alias = "IndexPageStateRx")]
+struct IndexPageState {
+    greeting: String,
+}
+
+#[auto_scope]
+fn index_page<G: Html>(cx: Scope, state: &IndexPageStateRx) -> View<G> {
+    view! { cx,
+        nav (class="drawer") {
+        input (id="nav-drawer", type="checkbox", class="drawer-toggle") {}
+        div (class="drawer-content flex flex-col") {
+          Navbar {}
+          div (class="page-content") {
+            h1 { "Hello world!" }
+          }
+        }
+        Drawer {}
+      }
+    }
+}
+
+#[engine_only_fn]
+fn head(cx: Scope, _props: IndexPageState) -> View<SsrNode> {
+    view! { cx,
+      title { "TheDrone7" }
+    }
+}
+
+#[engine_only_fn]
+async fn get_build_state(_info: StateGeneratorInfo<()>) -> IndexPageState {
+    IndexPageState {
+        greeting: "Hello world!".to_string(),
+    }
+}
+
+pub fn get_template<G: Html>() -> Template<G> {
+    Template::build("index")
+        .build_state_fn(get_build_state)
+        .view_with_state(index_page)
+        .head_with_state(head)
+        .build()
+}
