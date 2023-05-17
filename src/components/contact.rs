07d2e60e0972bd6438c412f0f524ca2e649f541c
TheDrone7 <h@thedrone7.dev>
2023-05-17 01:14:47 -0400

    Finish up with contact section

--- src/components/contact.rs
+++ src/components/contact.rs
@@ -1,4 +1,5 @@
 use super::icons::up::ChevronUp;
+use crate::components::icons::logos::{DiscIcon, GHubIcon, InstIcon, MailIcon, ReplIcon, TwitIcon};
 use sycamore::prelude::*;
 
 #[component]
@@ -7,9 +8,7 @@ pub fn ContactSection<G: Html>(cx: Scope) -> View<G> {
     let insta = "thedrone_7";
     let twitr = "TheDrone_7";
 
-    // Discord ID and name#tag
     let dscid = "374886124126208000";
-    let dcnam = "@TheDrone7#1624";
 
     // Extra links
     let replit = "TheDrone7";
@@ -26,8 +25,70 @@ pub fn ContactSection<G: Html>(cx: Scope) -> View<G> {
                     "Want to hire me or just talk to me? "
                     "Feel free to use any of the following options!"
                 }
-                div (class="grid grid-cols-1 grid-flow-row") {
+                div (class="grid grid-cols-1 grid-flow-row gap-4") {
+                    a (
+                        class="btn btn-success btn-outline btn-wide flex-row justify-start align-center pl-8",
+                        href=("mailto:".to_string() + email),
+                        target="_blank"
+                    ) {
+                        MailIcon {}
+                        div (class="divider divider-horizontal before:bg-success after:bg-success")
+                        span { "Email" }
+                    }
 
+                    a (
+                        class="btn btn-error btn-outline btn-wide flex-row justify-start align-center pl-8",
+                        href=("https://instagram.com/".to_string() + insta),
+                        target="_blank"
+                    ) {
+                        InstIcon {}
+                        div (class="divider divider-horizontal before:bg-error after:bg-error")
+                        span { "Instagram" }
+                    }
+
+                    a (
+                        class="btn btn-info btn-outline btn-wide flex-row justify-start align-center pl-8",
+                        href=("https://twitter.com/".to_string() + twitr),
+                        target="_blank"
+                    ) {
+                        TwitIcon {}
+                        div (class="divider divider-horizontal before:bg-info after:bg-info")
+                        span { "Twitter" }
+                    }
+
+                    a (
+                        class="btn btn-info btn-outline btn-wide flex-row justify-start align-center pl-8",
+                        href=("https://discord.com/users/".to_string() + dscid),
+                        target="_blank"
+                    ) {
+                        DiscIcon {}
+                        div (class="divider divider-horizontal before:bg-info after:bg-info")
+                        span { "Discord" }
+                    }
+                }
+                div (class="divider")
+                p (class="mb-4") {
+                    "Want to check out more of my work? Check these out!"
+                }
+                div (class="grid grid-cols-1 grid-flow-row gap-4") {
+                    a (
+                        class="btn btn-accent btn-outline btn-wide flex-row justify-start align-center pl-8",
+                        href=("https://replit.com/@".to_string() + replit),
+                        target="_blank"
+                    ) {
+                        ReplIcon {}
+                        div (class="divider divider-horizontal before:bg-accent after:bg-accent")
+                        span { "Replit" }
+                    }
+                    a (
+                        class="btn btn-outline btn-wide flex-row justify-start align-center pl-8",
+                        href=("https://github.com/".to_string() + github),
+                        target="_blank"
+                    ) {
+                        GHubIcon {}
+                        div (class="divider divider-horizontal before:bg-neutral-content after:bg-neutral-content")
+                        span { "GitHub" }
+                    }
                 }
             }
             div (class="absolute bottom-12 flex-row") {
