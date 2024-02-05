8bc20d601aa70465eeef92213e30c51e309f3c1b
TheDrone7 <hsbhatia765@gmail.com>
2024-02-05 02:04:36 -0500

    Add minimal header test

---
+++ src/components/__tests__/MinimalHeader.cy.ts
@@ -0,0 +1,22 @@
+import MinimalHeader from '../minimal/Header.vue';
+
+describe('Minimal Header', () => {
+  it('playground', () => {
+    cy.mount(MinimalHeader);
+  });
+
+  it('renders properly', () => {
+    cy.mount(MinimalHeader);
+    cy.get('header').should('exist');
+  });
+
+  it('contains the name(s)', () => {
+    cy.mount(MinimalHeader);
+    cy.get('header').contains('Harmeet Singh Bhatia').and('contain', 'TheDrone7');
+  });
+
+  it('contains the link to email', () => {
+    cy.mount(MinimalHeader);
+    cy.get('header a').should('have.length', 1).and('have.attr', 'href', 'mailto:h@thedrone7.dev');
+  });
+});
