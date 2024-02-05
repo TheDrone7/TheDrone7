17f9c6552292753dca3f255ba06633e93e0a8f62
TheDrone7 <hsbhatia765@gmail.com>
2024-02-05 18:34:30 -0500

    Update Projects section

--- src/components/minimal/Projects.vue
+++ src/components/minimal/Projects.vue
@@ -72,22 +72,29 @@ const projects = [
 <template>
   <section flex="~ col gap-4" pb-4 divider>
     <h2 text-xl font-bold>Other projects</h2>
-    <div flex="~ col gap-3">
-      <div v-for="project in projects" :key="project.title">
-        <div flex="~ row gap-1">
-          <h3 font-bold>{{ project.title }}</h3>
-          :
-          <p text-white-3 light:text-black-3>{{ project.desc }}</p>
-        </div>
-        <div flex="~ row gap-2">
-          <div flex="~ row gap-2 justify-center items-center" v-for="(link, i) in project.links" :key="i">
-            <a :href="link.link" target="_blank" rel="noopener noreferrer">
+    <table>
+      <tr v-for="project in projects" :key="project.title">
+        <th v-text="project.title" text="right" font-bold pr-4 border="r white-5/20 solid" />
+        <td pl-4 py-2>
+          <p v-text="project.desc" />
+          <div flex="~ row gap-2">
+            <a
+              v-for="(link, i) in project.links"
+              :key="link.text"
+              :href="link.link"
+              target="_blank"
+              rel="noopener noreferrer"
+              pr-2
+              :class="{ 'border-r border-solid border-white-5/10': i !== project.links.length - 1 }"
+            >
               {{ link.text }}
             </a>
-            <span v-if="i < project.links.length - 1" text-white-5:30 light:text-black-5:30>|</span>
           </div>
-        </div>
-      </div>
+        </td>
+      </tr>
+    </table>
+    <div flex="~ col gap-3">
+      <div v-for="project in projects" :key="project.title" flex="~ row gap-1"></div>
       <p font-italic text-white-5 light:text-black-5>There are probably more.</p>
     </div>
   </section>
