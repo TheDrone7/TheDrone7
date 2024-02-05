469375ee5556595457eb1a7bab6f3f0d43dbe494
TheDrone7 <hsbhatia765@gmail.com>
2024-02-05 01:56:22 -0500

    Finish footer test

--- src/components/__tests__/MinimalFooter.cy.ts
+++ src/components/__tests__/MinimalFooter.cy.ts
@@ -9,4 +9,15 @@ describe('Minimal Footer', () => {
     cy.mount(MinimalFooter);
     cy.get('footer').should('exist');
   });
+
+  it('contains the copyright', () => {
+    const currentYear = new Date().getFullYear();
+    cy.mount(MinimalFooter);
+    cy.get('footer').contains('© 2021').and('contain', currentYear.toString());
+  });
+
+  it('contains the link to socials', () => {
+    cy.mount(MinimalFooter);
+    cy.get('footer a').should('have.length', 4);
+  });
 });
